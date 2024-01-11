4c989e1c0f50a092967cad48f19e1e8b7b185b19
LiquidRetarded <141934856+LiquidRetarded@users.noreply.github.com>
2024-01-11 21:26:11 +0700

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -54,7 +54,7 @@ import kotlin.concurrent.thread
 object LiquidBounce {
 
     // Client informations
-    const val CLIENT_NAME = "LiquidBounce"
+    const val CLIENT_NAME = "LRClient"
     val clientVersionText = gitInfo["git.build.version"]?.toString() ?: "unknown"
     val clientVersionNumber = clientVersionText.substring(1).toIntOrNull() ?: 0 // version format: "b<VERSION>" on legacy
     val clientCommit = gitInfo["git.commit.id.abbrev"]?.let { "git-$it" } ?: "unknown"
