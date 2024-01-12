f9875c74f500d0f56c0268f40bc58d83b041e2bc
LiquidRetarded <tuilazerotwo@gmail.com>
2024-01-12 10:55:26 +0700

    Lb.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -54,17 +54,17 @@ import kotlin.concurrent.thread
 object LiquidBounce {
 
     // Client informations
-    const val CLIENT_NAME = "LRClient"
+    const val CLIENT_NAME = "LiquidBounce"
     val clientVersionText = gitInfo["git.build.version"]?.toString() ?: "unknown"
     val clientVersionNumber = clientVersionText.substring(1).toIntOrNull() ?: 0 // version format: "b<VERSION>" on legacy
     val clientCommit = gitInfo["git.commit.id.abbrev"]?.let { "git-$it" } ?: "unknown"
     val clientBranch = gitInfo["git.branch"]?.toString() ?: "unknown"
-    const val IN_DEV = true
+    const val IN_DEV = false
     const val CLIENT_CREATOR = "CCBlueX, TLZ"
     const val MINECRAFT_VERSION = "1.8.9"
     const val CLIENT_CLOUD = "https://cloud.liquidbounce.net/LiquidBounce"
 
-    val clientTitle = CLIENT_NAME + " " + clientVersionText + " " + clientCommit + "  | " + MINECRAFT_VERSION + if (IN_DEV) " | DEVELOPMENT BUILD" else ""
+    val clientTitle = CLIENT_NAME + " " + clientVersionText + "  | " + if (IN_DEV) " | DEVELOPMENT BUILD" else ""
 
     var isStarting = true
 
